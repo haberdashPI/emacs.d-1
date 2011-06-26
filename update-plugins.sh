1ce5e9ac278e9bab3d98fdb2eac4494495e34833
Antonio <antonio@santosvelasco.com>
2011-06-26 13:54:30 +0200

    Plugins updated

--- update-plugins.sh
+++ update-plugins.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-PWD=$(pwd)
+WORKING_DIR=$(pwd)
 
-cd pathogen && git pull origin master && cd $PWD
+cd pathogen && git pull origin master
+
+cd $WORKING_DIR
+
+echo "HELLO AT $(pwd)"
 
 cd vim/bundle && for d in $(ls); do cd $d; git pull origin master; cd ..; done
 
