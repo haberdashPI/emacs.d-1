cbcd6f5a19b7d4aafbdb30ebc65b0ac35c4ff0b2
Antonio Santos <antonio@santosvelasco.com>
2012-08-22 15:41:03 +0200

    Update plugins

--- update-plugins.sh
+++ update-plugins.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
-WORKING_DIR=$(pwd)
-
-cd pathogen && git pull origin master
-
-cd $WORKING_DIR
-
-cd vim/bundle && for d in $(ls); do cd $d; git pull origin master; cd ..; done
-
-cd $WORKING_DIR
-
-git add pathogen vim/bundle
-git commit -am "Plugins updated"
+git submodule foreach 'git pull origin master'
+git commit -am "Update plugins"
 
 echo "Vim plugins updated. Make it sure that you push the changes to the main repository"
