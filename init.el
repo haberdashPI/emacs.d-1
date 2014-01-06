d69a1476ca85802c279e309b10f3c52c50bc0e65
Antonio Santos <antonio@santosvelasco.com>
2014-01-06 01:06:15 +0100

    Move from .emacs to emacs.d/init.el

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
     (goto-char (point-max))
     (eval-print-last-sexp)))
 
-(add-to-list 'el-get-recipe-path "~/.emacs.d/recipes")
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get/recipes")
 (setq el-get-user-package-directory "~/.emacs.d/init-files/")
 (el-get 'sync)
 
@@ -111,15 +111,3 @@
       '(base16-theme))
 
 (el-get 'sync el-get-packages)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("e53cc4144192bb4e4ed10a3fa3e7442cae4c3d231df8822f6c02f1220a0d259a" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
