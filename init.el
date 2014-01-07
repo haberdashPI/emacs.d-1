02d79b4e8d94867a68a38266b79d848dba14b73b
Antonio Santos <antonio@santosvelasco.com>
2014-01-07 10:32:07 +0100

    Load the correct file

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-(load "custom.el")
+(load "~/.emacs.d/custom.el")
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
