f65f7fd17b73137be2cde7720966f6802d97d98f
Antonio Santos <antonio@santosvelasco.com>
2014-01-09 18:07:23 +0100

    Use latest el-get

--- init.el
+++ init.el
@@ -1,10 +1,12 @@
 (load "~/.emacs.d/custom.el")
 
-(setq package-archives '(("org" . "http://orgmode.org/elpa/")
+(require 'package)
+(setq package-archives '(
                          ("gnu" . "http://elpa.gnu.org/packages/")
+			 ("org" . "http://orgmode.org/elpa/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
-(package-initialize)
+                         ("melpa" . "http://melpa.milkbox.net/packages/")
+			))
 
 (setq el-get-user-package-directory "~/.emacs.d/init-files/")
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
@@ -13,8 +15,9 @@
   (with-current-buffer
       (url-retrieve-synchronously
        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
 
 (add-to-list 'el-get-recipe-path "~/.emacs.d/recipes")
 (el-get 'sync)
