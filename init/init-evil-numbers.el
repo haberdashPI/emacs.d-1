b64569bcb2589b98128cb93a9e6780d4a983728a
Antonio Santos <antonio@santosvelasco.com>
2014-01-16 10:19:09 +0100

    Change keybinds

--- init/init-evil-numbers.el
+++ init/init-evil-numbers.el
@@ -1,2 +1,2 @@
-(define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
-(define-key evil-normal-state-map (kbd "C-x") 'evil-numbers/dec-at-pt)
+(define-key evil-normal-state-map (kbd "C-x C-a") 'evil-numbers/inc-at-pt)
+(define-key evil-normal-state-map (kbd "C-x C-x") 'evil-numbers/dec-at-pt)
