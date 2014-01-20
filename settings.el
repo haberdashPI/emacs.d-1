cdb6da6d305dfdbb9c7ccdc67ff68839dee4cb54
Antonio Santos <antonio@santosvelasco.com>
2014-01-20 15:27:45 +0100

    Window movement

--- settings.el
+++ settings.el
@@ -65,3 +65,9 @@
 
 ;; automatically refresh files
 (global-auto-revert-mode t)
+
+;; window movement
+(global-set-key (kbd "M-h") 'windmove-left)
+(global-set-key (kbd "M-l") 'windmove-right)
+(global-set-key (kbd "M-k") 'windmove-up)
+(global-set-key (kbd "M-j") 'windmove-down)
