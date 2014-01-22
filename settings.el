7135d087d5c57667e8ddfd5981b53ce91f79149b
Antonio Santos <antonio@santosvelasco.com>
2014-01-22 17:29:43 +0100

    Disable autosave and backup, yolo

--- settings.el
+++ settings.el
@@ -25,18 +25,9 @@
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (setq inhibit-splash-screen t)
 
-;; autosave
-(defvar user-temporary-file-directory
-  (concat temporary-file-directory user-login-name "/"))
-(make-directory user-temporary-file-directory t)
-(setq backup-by-copying t)
-(setq backup-directory-alist
-      `(("." . ,user-temporary-file-directory)
-        (,tramp-file-name-regexp nil)))
-(setq auto-save-list-file-prefix
-      (concat user-temporary-file-directory ".auto-saves-"))
-(setq auto-save-file-name-transforms
-      `((".*" ,user-temporary-file-directory t)))
+;; disable auto-save and auto-backup
+(setq auto-save-default nil)
+(setq make-backup-files nil)
 
 ;; follow symlinks
 (setq vc-follow-symlinks t)
