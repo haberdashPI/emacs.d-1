b26593e1b701018b43efaa5163139744f3208289
Antonio Santos <antonio@santosvelasco.com>
2014-01-29 10:56:24 +0100

    More popup buffers

--- init/init-popwin.el
+++ init/init-popwin.el
@@ -1,5 +1,7 @@
 (require 'popwin)
 (popwin-mode 1)
 (setq popwin:popup-window-height 30)
+(push '("*" :regexp t :height 30) popwin:special-display-config)
 (push '("*helm" :regexp t :height 30) popwin:special-display-config)
 (push '("*compilation" :regexp t :height 50) popwin:special-display-config)
+(push '("*Bundler" :regexp t :height 50) popwin:special-display-config)
