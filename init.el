f4156539126bf669a486be9cffe684f3710cbce9
Antonio Santos <antonio@santosvelasco.com>
2014-03-04 23:30:42 +0100

    Replace el-get with cask and pallet

--- init.el
+++ init.el
@@ -1,72 +1,8 @@
+(require 'cask "~/.emacs.d/cask.el/cask.el")
+(cask-initialize)
+(require 'pallet)
+
 (load "~/.emacs.d/settings.el")
 (load "~/.emacs.d/keymap.el")
 (setq custom-file "~/.emacs.d/customize.el")
 (load custom-file)
-
-(mapc 'load (directory-files
-             (expand-file-name "~/.emacs.d/pre-init") t ".*\.el"))
-
-(require 'package)
-(setq package-archives '(
-                         ("gnu" . "http://elpa.gnu.org/packages/")
-                         ("org" . "http://orgmode.org/elpa/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
-                         ))
-
-(setq el-get-user-package-directory "~/.emacs.d/init/")
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (let (el-get-master-branch)
-      (goto-char (point-max))
-      (eval-print-last-sexp))))
-
-(add-to-list 'el-get-recipe-path "~/.emacs.d/recipes")
-(el-get 'sync)
-
-(setq el-get-packages '(
-                        auto-complete
-                        auto-complete-css
-                        bookmark+
-                        bundler
-                        coffee-mode
-                        dash-at-point
-                        ethan-wspace
-                        evil
-                        evil-leader
-                        evil-surround
-                        evil-numbers
-                        evil-nerd-commenter
-                        evil-matchit
-                        flycheck
-                        flymake-coffee
-                        fixme-mode
-                        git-blame
-                        haml-mode
-                        helm
-                        helm-ag
-                        helm-projectile
-                        inf-ruby
-                        json-mode
-                        markdown-mode
-                        org
-                        projectile
-                        popwin
-                        rainbow-mode
-                        rinari
-                        robe
-                        ruby-mode
-                        ruby-test-mode
-                        scss-mode
-                        slim-mode
-                        smartparens
-                        web-mode
-                        yaml-mode
-                        yasnippet
-                        ))
-
-(el-get 'sync el-get-packages)
