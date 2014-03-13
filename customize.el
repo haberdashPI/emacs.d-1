e007de6fd1137a13170b75c89dbea8b4fee91410
Antonio Santos <antonio@santosvelasco.com>
2014-03-13 12:12:06 +0100

    base16-tomorrow-dark is a safe theme

--- customize.el
+++ customize.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
- '(custom-safe-themes (quote ("e53cc4144192bb4e4ed10a3fa3e7442cae4c3d231df8822f6c02f1220a0d259a" default))))
+ '(custom-safe-themes (quote ("ea605f54caf4b40e044c4bb553924dab30d1b110aa801c238916fbcfe5a8ca09" "e53cc4144192bb4e4ed10a3fa3e7442cae4c3d231df8822f6c02f1220a0d259a" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
