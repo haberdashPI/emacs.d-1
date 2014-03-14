935afb6b653427d1defc9f907066f8ff844a9f40
Antonio Santos <antonio@santosvelasco.com>
2014-03-14 13:18:32 +0100

    Make it possible to cycle between apps with the same name

--- slate.js
+++ slate.js
@@ -20,21 +20,52 @@ focusWindow = function(title) {
   return findAndFocusWindow;
 };
 
-fullScreen = function() {
+var fullScreen = function() {
   slate.operation("corner", { direction: "top-left"}).run();
   slate.operation("resize", { width: "+100%", height: "+100%", anchor: "top-left" }).run();
 };
 
+var focusWindowWithPid = function(pid) {
+    slate.eachApp(function(app) {
+        if (app.pid() === pid) {
+            app.mainWindow().focus();
+        }
+    });
+}
+
 var focusLastWindow = function() {
   slate.operation("focus", { app: Config.last_window }).run();
 };
+
+var cycleBetweenWindowsOfTheSameApp = function(appName) {
+  slate.eachApp(function(app) {
+    var pids = [];
+    if (app.name() === appName) {
+      pids.push(app.pid());
+    }
+    pids.sort();
+    if (slate.app().name() !== appName) {
+      focusWindowWithPid(pids[0]);
+    } else {
+        currentAppPid = slate.app().pid();
+        i = pids.indexOf(currentAppPid);
+        if (i === (pids.length - 1)) {
+            focusWindowWithPid(pids[0]);
+        } else {
+            focusWindowWithPid(pids[i+1]);
+        }
+    }
+  });
+};
+
+var cycleBetweenEmacs = function() { cycleBetweenWindowsOfTheSameApp("Emacs") ; }
 
 slate.bind("1:cmd", slate.operation("focus", { app: "Google Chrome" }));
 slate.bind("2:cmd", slate.operation("focus", { app: "GitHub Chat" }));
 slate.bind("7:cmd", slate.operation("focus", { app: "iTunes" }));
 slate.bind("8:cmd", slate.operation("focus", { app: "Adium" }));
 slate.bind("9:cmd", slate.operation("focus", { app: "iTerm" }));
-slate.bind("e:cmd", slate.operation("focus", { app: "Emacs" }));
+slate.bind("e:cmd", cycleBetweenEmacs);
 slate.bind("esc:cmd", focusLastWindow);
 slate.bind("return:cmd", fullScreen);
 
