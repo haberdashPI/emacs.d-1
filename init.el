3a8325d37af956f5ed3f5cdd64f8a27c37a007b3
Antonio Santos <antonio@santosvelasco.com>
2014-03-16 21:37:19 +0100

    TODO states

--- init.el
+++ init.el
@@ -107,6 +107,9 @@
     (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
     (setq org-mobile-inbox-for-pull "~/Dropbox/Apps/MobileOrg/")
 
+    (setq org-todo-keywords
+        '((sequence "TODO(t)" "WAIT(w@/!)" "FUTURE(f)" "|" "DONE(d)" "CANCELED(c@)")))
+
     (global-set-key "\C-cl" 'org-store-link)
     (global-set-key "\C-ca" 'org-agenda)
     (global-set-key "\C-cb" 'org-iswitchb)))
