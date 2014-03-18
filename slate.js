526c84246c67cc34bf8bb9ece3b8166809ce1606
Antonio Santos <antonio@santosvelasco.com>
2014-03-18 16:15:50 +0100

    Do not redefine the pids array

--- slate.js
+++ slate.js
@@ -1,64 +1,64 @@
 Config = {
-    lastAppPid: slate.app().pid()
+  lastAppPid: slate.app().pid()
 };
 
 var relaunch = slate.operation("relaunch");
 slate.bind("r:cmd,ctrl", relaunch);
 
 focusWindow = function(title) {
-    findAndFocusWindow = function() {
-        var windowFound = false;
-        slate.eachApp(function(app) {
-            app.eachWindow(function(window) {
-                if (!windowFound && window.title() === title) {
-                    windowFound = true;
-                    window.focus();
-                }
-            });
-        });
-    };
-    return findAndFocusWindow;
+  findAndFocusWindow = function() {
+    var windowFound = false;
+    slate.eachApp(function(app) {
+      app.eachWindow(function(window) {
+        if (!windowFound && window.title() === title) {
+          windowFound = true;
+          window.focus();
+        }
+      });
+    });
+  };
+  return findAndFocusWindow;
 };
 
 var fullScreen = function() {
-    slate.operation("corner", { direction: "top-left"}).run();
-    slate.operation("resize", { width: "+100%", height: "+100%", anchor: "top-left" }).run();
+  slate.operation("corner", { direction: "top-left"}).run();
+  slate.operation("resize", { width: "+100%", height: "+100%", anchor: "top-left" }).run();
 };
 
 var focusWindowWithPid = function(pid) {
-    slate.eachApp(function(app) {
-        if (app.pid() === pid) {
-            app.mainWindow().focus();
-        }
-    });
+  slate.eachApp(function(app) {
+    if (app.pid() === pid) {
+      app.mainWindow().focus();
+    }
+  });
 }
 
 var focusLastWindow = function() {
-    focusWindowWithPid(Config.lastAppPid);
+  focusWindowWithPid(Config.lastAppPid);
 };
 
-var cycleBetweenWindowsOfTheSameApp = function(appName) {
-    slate.eachApp(function(app) {
-        var pids = [];
-        if (app.name() === appName) {
-            pids.push(app.pid());
-        }
-        pids.sort();
-        if (slate.app().name() !== appName) {
-            focusWindowWithPid(pids[0]);
-        } else {
-            currentAppPid = slate.app().pid();
-            i = pids.indexOf(currentAppPid);
-            if (i === (pids.length - 1)) {
-                focusWindowWithPid(pids[0]);
-            } else {
-                focusWindowWithPid(pids[i+1]);
-            }
-        }
-    });
+var cycleBetweenWindowsOfTheSameApp = function(appName, pids) {
+  slate.eachApp(function(app) {
+    if (app.name() === appName) {
+      pids.push(app.pid());
+    }
+    pids.sort();
+    slate.log(pids.length);
+    if (slate.app().name() !== appName) {
+      focusWindowWithPid(pids[0]);
+    } else {
+      currentAppPid = slate.app().pid();
+      i = pids.indexOf(currentAppPid);
+      if (i === (pids.length - 1)) {
+        focusWindowWithPid(pids[0]);
+      } else {
+        focusWindowWithPid(pids[i+1]);
+      }
+    }
+  });
 };
 
-var cycleBetweenEmacs = function() { cycleBetweenWindowsOfTheSameApp("Emacs") ; }
+var cycleBetweenEmacs = function() { cycleBetweenWindowsOfTheSameApp("Emacs", []); }
 
 slate.bind("1:cmd", slate.operation("focus", { app: "Google Chrome" }));
 slate.bind("2:cmd", slate.operation("focus", { app: "GitHub Chat" }));
@@ -70,5 +70,5 @@ slate.bind("esc:cmd", focusLastWindow);
 slate.bind("return:cmd", fullScreen);
 
 slate.on("appDeactivated", function(event, app) {
-    Config.lastAppPid = app.pid();
+  Config.lastAppPid = app.pid();
 });
