46e4351fca54ed931fad23dadda1e8323b9eae75
Antonio Santos <antonio@santosvelasco.com>
2014-03-24 17:29:42 +0100

    Define org archive

--- init.el
+++ init.el
@@ -138,6 +138,8 @@
 
     (setq org-default-notes-file (concat org-directory "/inbox.org"))
     (define-key global-map "\C-cc" 'org-capture)
+
+    (setq org-archive-location "~/Dropbox/org/archives/%s::datetree/")
     ))
 
 (use-package popwin
