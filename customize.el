509685577f970a5427057a16b156a66282c9a7da
Antonio Santos <antonio@santosvelasco.com>
2014-04-21 12:06:26 +0200

    Customize helm-source-header face

--- customize.el
+++ customize.el
@@ -11,10 +11,13 @@
  '(custom-safe-themes
    (quote
     ("5ae8f7212ee110e161e54c14ac6730508ac2de722aa38272b7fb1ee55a6f21e1" "ea605f54caf4b40e044c4bb553924dab30d1b110aa801c238916fbcfe5a8ca09" "e53cc4144192bb4e4ed10a3fa3e7442cae4c3d231df8822f6c02f1220a0d259a" default)))
+ '(helm-match-plugin-mode t nil (helm-match-plugin))
+ '(helm-mode t)
  '(org-agenda-files (quote ("~/Dropbox/org/plan.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#282a2e" :foreground "#e0e0e0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "apple" :family "Inconsolata for Powerline")))))
+ '(default ((t (:inherit nil :stipple nil :background "#282a2e" :foreground "#e0e0e0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "apple" :family "Inconsolata for Powerline"))))
+ '(helm-source-header ((t (:background "#22083397778B" :foreground "white" :family "Sans Serif")))))
