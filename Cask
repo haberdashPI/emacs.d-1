eb1861e5f017f00503d564ff4e3439b47123c419
Antonio Santos <antonio@santosvelasco.com>
2014-06-20 06:17:57 +0200

    Remove hrb-mode

--- Cask
+++ Cask
@@ -27,7 +27,6 @@
 (depends-on "helm-ag")
 (depends-on "helm-gtags")
 (depends-on "helm-projectile")
-(depends-on "hrb-mode" :git "https://github.com/ckruse/hrb-mode")
 (depends-on "inf-ruby")
 (depends-on "json-mode")
 (depends-on "magit")
--- init.el
+++ init.el
@@ -155,15 +155,6 @@
       )
     ))
 
-(use-package hrb-mode
-  :defer t
-  :config (progn
-            (setq hrb-delay 0)
-            (setq hrb-highlight-keyword-face 'show-paren-match-face)
-            (setq hrb-highlight-block-face 'highlight)
-            (setq hrb-highlight-mode 'keywords)
-            (add-hook 'ruby-mode-hook 'hrb-mode)))
-
 (use-package imenu+
   :defer t
   :config (progn
