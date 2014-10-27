77a9e2fb8848c78dab5d2d816fd628fcb8a7300e
Antonio Santos <antonio@santosvelasco.com>
2014-10-27 07:21:42 +0100

    org agenda window setup

--- init.el
+++ init.el
@@ -217,6 +217,7 @@
     (global-set-key "\C-cl" 'org-store-link)
     (global-set-key "\C-ca" 'org-agenda)
 
+    (setq org-agenda-window-setup 'current-window)
     (setq org-default-notes-file (concat org-directory "/plan.org"))
     (define-key global-map "\C-cc" 'org-capture)
 
