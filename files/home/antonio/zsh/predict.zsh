3c0b91ffa8d2502db2632170c7edb1ad657a7936
Antonio Santos <antonio@santosvelasco.com>
2015-02-09 12:30:57 +0100

    Make prediction work with history search

--- files/home/antonio/zsh/predict.zsh
+++ files/home/antonio/zsh/predict.zsh
@@ -7,8 +7,27 @@ antonio-accept-line () {
   zle .kill-line
   zle .accept-line
 }
-
 zle -N antonio-accept-line
+
+antonio-history-incremental-accept-line () {
+  zle .accept-line
+  bindkey '^M' antonio-accept-line
+}
+zle -N antonio-history-incremental-accept-line
+
+antonio-history-incremental-search-backward () {
+  bindkey '^M' antonio-history-incremental-accept-line
+  zle .history-incremental-search-backward
+}
+zle -N antonio-history-incremental-search-backward
+
+antonio-history-incremental-search-forward () {
+  bindkey '^M' antonio-history-incremental-accept-line
+  zle .history-incremental-search-forward
+}
+zle -N antonio-history-incremental-search-forward
 
 bindkey '^M' antonio-accept-line
 bindkey '^[^M' .accept-line
+bindkey '^R' antonio-history-incremental-search-backward
+bindkey '^S' antonio-history-incremental-search-forward
