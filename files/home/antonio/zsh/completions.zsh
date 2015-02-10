07f3e46a75b6f4577f9721d5bb1106d933584fa9
Antonio Santos <antonio@santosvelasco.com>
2015-02-10 08:12:01 +0100

    Add some docs

--- files/home/antonio/zsh/completions.zsh
+++ files/home/antonio/zsh/completions.zsh
@@ -12,6 +12,8 @@ setopt complete_in_word         # allow completion from within a word/phrase
 setopt correct                  # spelling correction for commands
 setopt list_ambiguous           # complete as much of a completion until it gets ambiguous.
 
+# Format is :completion:function:completer:command:argument:tag.
+
 zstyle ':completion::complete:*' use-cache on               # completion caching, use rehash to clear
 zstyle ':completion:*' cache-path ~/.zsh/cache              # cache path
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'   # ignore case
