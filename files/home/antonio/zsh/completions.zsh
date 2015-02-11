f791fb8c00504f54a3cbe34196b14ab43a0ff6f4
Antonio Santos <antonio@santosvelasco.com>
2015-02-11 07:22:09 +0100

    Use prezto completions module

--- files/home/antonio/zsh/completions.zsh
+++ files/home/antonio/zsh/completions.zsh
@@ -1,22 +1,6 @@
 #!/bin/zsh
 
 fpath+=(~/.zsh/modules/zsh-completions/src)
-
-autoload -U compinit && compinit
-
-zmodload -i zsh/complist
-setopt hash_list_all            # hash everything before completion
-setopt completealiases          # complete alisases
-setopt always_to_end            # when completing from the middle of a word, move the cursor to the end of the word
-setopt complete_in_word         # allow completion from within a word/phrase
-setopt correct                  # spelling correction for commands
-setopt list_ambiguous           # complete as much of a completion until it gets ambiguous.
-
-# Format is :completion:function:completer:command:argument:tag.
-
-zstyle ':completion::complete:*' use-cache on               # completion caching, use rehash to clear
-zstyle ':completion:*' cache-path ~/.zsh/cache              # cache path
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'   # ignore case
-zstyle ':completion:*' menu select=2                        # menu if nb items > 2
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}       # colorz !
-zstyle ':completion:*::::' completer _expand _complete _ignored _approximate # list of completers to use
+source ~/.zsh/modules/prezto/modules/completion/init.zsh
+setopt no_completealiases
+compinit
