b542fa3ef3fc7dddd05011d76f73ee30fabec82b
Antonio Santos <antonio@santosvelasco.com>
2015-03-20 09:02:52 +0100

    Turn off/on fci-mode when popups are created/deleted

--- files/home/antonio/emacs.d/init.el
+++ files/home/antonio/emacs.d/init.el
@@ -273,6 +273,17 @@
   :init
   (progn
     (add-hook 'prog-mode-hook 'fci-mode)
+    (defvar sanityinc/fci-mode-suppressed nil)
+    (defadvice popup-create (before suppress-fci-mode activate)
+      "Suspend fci-mode while popups are visible"
+      (set (make-local-variable 'sanityinc/fci-mode-suppressed) fci-mode)
+      (when fci-mode
+        (turn-off-fci-mode)))
+    (defadvice popup-delete (after restore-fci-mode activate)
+      "Restore fci-mode when all popups have closed"
+      (when (and (not popup-instances) sanityinc/fci-mode-suppressed)
+        (setq sanityinc/fci-mode-suppressed nil)
+        (turn-on-fci-mode)))
     (setq fci-rule-color "#333333"
           fci-rule-column 80)))
 
