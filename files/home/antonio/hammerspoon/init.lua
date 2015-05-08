5b79407c97a3c6ad60cbd42d7dc7e854565e89a3
Antonio Santos <antonio@santosvelasco.com>
2015-05-08 07:24:23 +0200

    Replace Spectacle with Hammerspoon

--- files/home/antonio/hammerspoon/init.lua
+++ files/home/antonio/hammerspoon/init.lua
@@ -1,8 +1,10 @@
+hs.window.animationDuration = 0
+
 -- what i want is a modal keymap, like I do cmd and then cmd +
 -- something has a different functionality
 
 -- full screen
-hs.hotkey.bind({"cmd", "alt"}, "return", function ()
+hs.hotkey.bind({"cmd", "shift"}, "return", function ()
     local win    = hs.window.focusedWindow()
     local f      = win:frame()
     local max    = win:screen():frame()
@@ -15,6 +17,54 @@ hs.hotkey.bind({"cmd", "alt"}, "return", function ()
 end)
 
 -- move to the left/right/down/up side
+hs.hotkey.bind({"cmd", "shift"}, "h", function ()
+    local win    = hs.window.focusedWindow() or hs.application.frontmostApplication():focusedWindow()
+    local f      = win:frame()
+    local max    = win:screen():frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "j", function ()
+    local win    = hs.window.focusedWindow() or hs.application.frontmostApplication():focusedWindow()
+    local f      = win:frame()
+    local max    = win:screen():frame()
+
+    f.x = max.x
+    f.y = max.h / 2
+    f.w = max.w
+    f.h = max.h / 2
+    win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "k", function ()
+    local win    = hs.window.focusedWindow() or hs.application.frontmostApplication():focusedWindow()
+    local f      = win:frame()
+    local max    = win:screen():frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w
+    f.h = max.h / 2
+    win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "l", function ()
+    local win    = hs.window.focusedWindow() or hs.application.frontmostApplication():focusedWindow()
+    local f      = win:frame()
+    local max    = win:screen():frame()
+
+    f.x = max.w / 2
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
+end)
+
 -- move to the next windows with cmd+j, cmd+k
 -- caffeine replacement
 -- cycle between windows of the same app
